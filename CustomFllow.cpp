c06a55e4e2e381e42ae1d3fcf0290d187a648b27
Ryo Ishikawa <ryo31330023@gmail.com>
2015-03-23 18:16:10 +0900

    added file

---
+++ CustomFllow.cpp
@@ -0,0 +1,93 @@
+//  CustomFllow.cpp
+//
+//  Created by rinov.
+//
+
+#include "CustomFllow.h"
+
+USING_NS_CC;
+
+CustomFollow::CustomFollow(){
+    _type = ScrollType::Default;
+    _margin = Rect::ZERO;
+}
+
+CustomFollow::~CustomFollow(){
+    CC_SAFE_RELEASE_NULL(_followedNode);
+}
+
+CustomFollow* CustomFollow::create(Node* followNode){
+    CustomFollow *follow = new CustomFollow();
+    if (follow && follow->initWithTarget(followNode, Rect::ZERO))
+    {
+        follow->autorelease();
+        return follow;
+    }
+    if(follow){
+        delete follow;
+        follow=nullptr;
+    }
+
+    return nullptr;
+}
+
+CustomFollow* CustomFollow::create(Node* followNode,ScrollType type){
+    CustomFollow *follow = new CustomFollow();
+    follow->_type = type;
+    if (follow && follow->initWithTarget(followNode, Rect::ZERO))
+    {
+        follow->autorelease();
+        return follow;
+    }
+    if(follow){
+        delete follow;
+        follow=nullptr;
+    }
+    return nullptr;
+}
+
+void CustomFollow::step(float dt)
+{
+    CC_UNUSED_PARAM(dt);
+    
+    if(_boundarySet){
+        
+        if(_boundaryFullyCovered){
+            return;
+        }
+        
+        Point t = _halfScreenSize - _followedNode->getPosition();
+        
+        float x = clampf(t.x, _leftBoundary, _rightBoundary);
+        float y = clampf(t.y, _bottomBoundary, _topBoundary);
+        
+        if(_type == ScrollType::OnlyX){
+            y = _target->getPositionY();
+        }
+        else if (_type == ScrollType::OnlyY){
+            x = _target->getPositionX();
+        }
+        
+        _target->setPosition(Point(x , y));
+        
+    }else{
+        
+        Point p = _target->convertToWorldSpace(_followedNode->getPosition());
+        
+        if(_margin.containsPoint(p)){
+            return;
+        }
+        
+        float x = _halfScreenSize.x - _followedNode->getPositionX();
+        float y = _halfScreenSize.y - _followedNode->getPositionY();
+        
+        if(_type == ScrollType::OnlyX){
+            y = _target->getPositionY();
+        }
+        else if (_type == ScrollType::OnlyY){
+            x = _target->getPositionX();
+        }
+        
+        _target->setPosition(Point(x , y));
+    }
+}
---
+++ CustomFllow.h
@@ -0,0 +1,34 @@
+//  CustomFllow.h
+//
+//  Created by rinov.
+//
+
+#ifndef __COCOS2DX__CustomFllow__
+#define __COCOS2DX__CustomFllow__
+
+#include "cocos2d.h"
+
+enum class ScrollType{
+    OnlyX,
+    OnlyY,
+    Default,
+};
+
+class CustomFollow : public cocos2d::Follow
+{
+public:
+    
+    static CustomFollow* create(cocos2d::Node *followedNode);
+    static CustomFollow* create(cocos2d::Node *followedNode,ScrollType type);
+    virtual void step(float dt) override;
+
+private:
+
+    CustomFollow();
+    virtual ~CustomFollow();
+    ScrollType _type;
+    cocos2d::Rect _margin;
+
+};
+
+#endif
